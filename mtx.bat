4d527a57e573967ad14b68ddf2c37b94a4b4baac
Fernando Manfredi <ranthrall@gmail.com>
2025-02-12 01:02:40 -0300

    Update mtx.bat

--- mtx.bat
+++ mtx.bat
@@ -4,5 +4,5 @@ color 02
 cls
 :start
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-ping 127.0.0.1 -n 1 -w 5000 >NUL
+ping 127.0.0.1 -n 1 -w 50 >NUL
 goto start
