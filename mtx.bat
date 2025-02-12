6fa8d15457441487104b8f1f2a22846d40974750
Fernando Manfredi <ranthrall@gmail.com>
2025-02-12 01:00:42 -0300

    Create mtx.bat

---
+++ mtx.bat
@@ -0,0 +1,8 @@
+title MATRIX
+echo off
+color 02
+cls
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+ping 127.0.0.1 -n 1 -w 5000 >NUL
+goto start
